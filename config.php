3aad15e41fa09dd07665f0f2b2b96d96d88c8f76
h05011689-hash <h05011689@gmail.com>
2026-06-04 06:15:45 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$API_KEY = "8795620005:AAH_1jHY2skQQNkrRN2z9DIa6XVt4imogss";//توكن البوت
+$token = $API_KEY;
+$API_SU = file_get_contents("apifastpva.txt");//توكن موقع الأرقام supersms.ml
+$API_APP = file_get_contents("app.txt");//توكن موقع الأرقام supersms.ml
+$admin = array(8147516847);//ايدي المطور
+$me =-1003702925187;/*
+$me : 
+هون بتقدر تحط الايدي تبعك
+او تحط ايدي قناة
+بيرسل عليها الأرقام
+
+*/
+?>
+
+
+
+
